07363ef6639d2f85a179b6df924699776f707b62
陈晓辉 <529912921@qq.com>
2018-10-08 19:11:39 +0800

    更换 icon

---
+++ CXChangeAppIcon/UIViewController+Present.m
@@ -0,0 +1,48 @@
+//
+//  UIViewController+Present.m
+//  DynamicAppIcon
+//
+//  Created by daiyi on 2017/5/1.
+//  Copyright © 2017年 DY. All rights reserved.
+//
+
+#import "UIViewController+Present.h"
+#import <objc/runtime.h>
+
+@implementation UIViewController (Present)
+
+//拦截弹框
+
+//每次更换 icon 时系统会自动弹出提示框, 如不需要, 怎使用该类进行拦截
++ (void)load {
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Method presentM = class_getInstanceMethod(self.class, @selector(presentViewController:animated:completion:));
+        Method presentSwizzlingM = class_getInstanceMethod(self.class, @selector(dy_presentViewController:animated:completion:));
+        
+        method_exchangeImplementations(presentM, presentSwizzlingM);
+    });
+}
+
+- (void)dy_presentViewController:(UIViewController *)viewControllerToPresent animated:(BOOL)flag completion:(void (^)(void))completion {
+    
+    if ([viewControllerToPresent isKindOfClass:[UIAlertController class]]) {
+        NSLog(@"title : %@",((UIAlertController *)viewControllerToPresent).title);
+        NSLog(@"message : %@",((UIAlertController *)viewControllerToPresent).message);
+        
+        UIAlertController *alertController = (UIAlertController *)viewControllerToPresent;
+        if (alertController.title == nil && alertController.message == nil) {
+            return;
+        } else {
+            [self dy_presentViewController:viewControllerToPresent animated:flag completion:completion];
+            return;
+        }
+    }
+    
+    [self dy_presentViewController:viewControllerToPresent animated:flag completion:completion];
+}
+
+
+@end
+
--- CXChangeAppIcon/ViewController.m
+++ CXChangeAppIcon/ViewController.m
@@ -10,14 +10,44 @@
 
 @interface ViewController ()
 
+/** icon 数组 */
+@property (nonatomic, copy) NSArray<NSString *> *weathers;
+
 @end
 
 @implementation ViewController
 
+#pragma mark ---------- 更换图标功能（API）当前只支持iOS10.3以上的系统 ----------
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //https://www.jianshu.com/p/dd5144f8f021
+    self.weathers = @[@"晴", @"多云", @"小雨", @"大雨", @"雪", @""];
 }
 
+- (IBAction)changeAppIcon:(id)sender {
+    
+    NSString *weather = self.weathers[arc4random() % self.weathers.count];
+    [self setAppIconWithName:weather];
+}
+
+- (void)setAppIconWithName:(NSString *)iconName {
+    
+    //是否支持更换 icon
+    if (![[UIApplication sharedApplication] supportsAlternateIcons]) {
+        return;
+    }
+    
+    if ([iconName isEqualToString:@""]) {
+        iconName = nil;
+    }
+    
+    //iconName = nil 时, 使用主图标
+    [[UIApplication sharedApplication] setAlternateIconName:iconName completionHandler:^(NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"更换app图标发生错误了 ： %@",error);
+        }
+    }];
+}
 
 @end
